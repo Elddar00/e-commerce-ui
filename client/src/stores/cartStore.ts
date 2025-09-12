c4927ce60cdeefdde6810011994d049bcb93c2f7
Elddar00 <eldaridrizovic00@gmail.com>
2025-09-12 14:13:38 +0200

    Fix Zustand Hydration Problem

--- client/src/stores/cartStore.ts
+++ client/src/stores/cartStore.ts
@@ -6,6 +6,7 @@ const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
   persist(
     (set) => ({
       cart: [],
+      hasHydrated: false,
       addToCart: (product) =>
         set((state) => {
           const existingIndex = state.cart.findIndex(
@@ -44,7 +45,15 @@ const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
         })),
       clearCart: () => set({ cart: [] }),
     }),
-    { name: "cart", storage: createJSONStorage(() => localStorage) }
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.hasHydrated = true;
+        }
+      },
+    }
   )
 );
 
